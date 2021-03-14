360c84ebebc0e33adabc55355f44676ae4954385
Isaac Cabello <cabelloisaac97@gmaill.com>
2021-03-14 17:51:04 -0500

    Added validation to login with email and password

--- lib/ui/screens/auth/login/widgets/email_and_password_login.dart
+++ lib/ui/screens/auth/login/widgets/email_and_password_login.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 
 class EmailAndPasswordLogin extends StatelessWidget {
-  const EmailAndPasswordLogin({
-    Key key,
-  }) : super(key: key);
+  final _formKey = GlobalKey<FormState>();
+  bool _fieldsValidated = false;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(labelText: "Email"),
-        ),
-        TextField(
-          decoration: InputDecoration(labelText: "Password"),
-        ),
-        SizedBox(height: 20),
-        SizedBox(
-          width: double.infinity,
-          child: ElevatedButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.email),
-            label: Text("Sign in with email"),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            validator: _validatorEmail,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(labelText: "Email"),
           ),
-        ),
-      ],
+          TextFormField(
+            validator: _validatorPassword,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(labelText: "Password"),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.email),
+              label: Text("Sign in with email"),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  String _validatorEmail(String value) {
+    if (value.trim().isEmpty) {
+      _fieldsValidated = false;
+      return "Please, enter your email address";
+    }
+    return null;
+  }
+
+  String _validatorPassword(String value) {
+    if (value.trim().isEmpty) {
+      _fieldsValidated = false;
+      return "Please, enter your password";
+    }
+    return null;
   }
 }
