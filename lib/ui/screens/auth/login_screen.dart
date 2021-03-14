f62db6e0a690886c9cfefb2110a3d81c6b4417e5
Isaac Cabello <cabelloisaac97@gmaill.com>
2021-03-14 17:33:11 -0500

    Working on login screen

--- lib/ui/screens/auth/login_screen.dart
+++ lib/ui/screens/auth/login_screen.dart
@@ -4,6 +4,44 @@ class LoginScreen extends StatelessWidget {
   static final route = "/LoginScreen";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(40),
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset("assets/images/spotify.png", height: 100, width: 100),
+              Text("Welcome Back"),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.g_translate),
+                      label: Text("Google"),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.g_translate),
+                      label: Text("Facebook"),
+                    ),
+                  ),
+                ],
+              ),
+              Divider(),
+              TextField(
+                decoration: InputDecoration(labelText: "Email"),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Password"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
