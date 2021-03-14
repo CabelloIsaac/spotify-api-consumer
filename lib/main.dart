f52a6a2d0d4ecf02a34639bf221cf05e33e2b621
Isaac Cabello <cabelloisaac97@gmaill.com>
2021-03-14 17:25:00 -0500

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Spotify Api Consumer',
       routes: MyRouter.routes,
       initialRoute: LoginScreen.route,
